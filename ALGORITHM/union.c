07bdcc8939f7e999d3bd864cff43835dbd5bec90
KimUJin3359 <wj3359@naver.com>
2021-07-05 22:53:44 +0900

    210705

---
+++ ALGORITHM/union.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+int main(int ac, char *av[])
+{
+	char judge[256];
+
+	if (ac != 3)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	for (int i = 0; i < 256; i++)
+		judge[i] = 0;
+	for (int i = 0; av[1][i]; i++)
+		judge[av[1][i]] = 1;
+	for (int i = 0; av[2][i]; i++)
+		judge[av[2][i]] = 1;
+	for (int i = 0; av[1][i]; i++)
+	{
+		if (judge[av[1][i]])
+		{
+			write(1, (const void *)(&av[1][i]), 1);
+			judge[av[1][i]] = 0;
+		}
+	}
+	for (int i = 0; av[2][i]; i++)
+	{
+		if (judge[av[2][i]])
+		{
+			write(1, (const void *)(&av[2][i]), 1);
+			judge[av[2][i]] = 0;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
